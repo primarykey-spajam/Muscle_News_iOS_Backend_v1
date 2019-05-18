7b0304370a0f6635218c49078c4cf8644803c07c
Yuta Shimomura <yutashimomura@YutanoMacBook-Pro.local>
2019-05-19 00:03:40 +0900

    creating model

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class User(models.Model):
+    userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    anqResult = models.CharField(max_length=30)
+
+class Muscle(models.Model):
+    newsId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    newsText = models.TextField()
+    newsVoice = models.FileField()
+    textType = models.CharField(max_length=30)
--- muscle_news_backend/settings.py
+++ muscle_news_backend/settings.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api"
+    "rest_frammework",
 ]
 
 MIDDLEWARE = [
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
