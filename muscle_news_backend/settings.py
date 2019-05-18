599eadd1bdcf54858c7506aff7790392130062fd
Yuta Shimomura <yutashimomura@YutanoMacBook-Pro.local>
2019-05-19 00:06:59 +0900

    migrate

--- muscle_news_backend/settings.py
+++ muscle_news_backend/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "api"
-    "rest_frammework",
+    "api",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
